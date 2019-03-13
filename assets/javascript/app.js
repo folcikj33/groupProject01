1d6eeda1163b7d0fb85bb63989a67a6e18687793
Liz Lemon <nikorasu.dx@gmail.com>
2019-03-13 14:16:16 -0500

    add color

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,15 +9,19 @@ $.ajax({
     method: "GET"
 }).then(function (response) {
     // p tag needed?
-    $("#city-details").append(Math.round(response.teleport_city_score) + " out of 100.", response.summary);
+    $("#city-details").append("<strong>Overall City Score: </strong>" + Math.round(response.teleport_city_score) + " out of 100.<hr>", response.summary);
     for (let i = 0; i < response.categories.length; i++) {
         let detailName = response.categories[i].name;
         console.log(response.categories[0])
         console.log(detailName)
         let detailScore = response.categories[i].score_out_of_10;
         let detailElement = $("<p>");
+        // add color
+        let color = response.categories[i].color
+        $(detailElement).attr("style", "color: " + color)
         $(detailElement).append(detailName + ": " + detailScore.toFixed(1) + " (out of 10)")
         $("#city-details").append(detailElement)
+
     }
 
 });
