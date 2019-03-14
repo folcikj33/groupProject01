fbeeec1123ae93da8675163cf00c23ac7d66541a
Liz Lemon <nikorasu.dx@gmail.com>
2019-03-14 13:50:27 -0500

    adding cities and city search feature

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,52 @@
 $(document).ready(function () {
-    console.log(document);
-
-
-    let searchCity = "chicago";
-    let queryURL = "https://api.teleport.org/api/urban_areas/slug%3A" + searchCity + "/scores/"
-
     $("#submit-button").on("click", function (event) {
         event.preventDefault();
         console.log("this got click");
+        $("#city-details").empty()
+
+        let citySelected = $("#city-selected").find(":selected").text();
+        let apiCities = {
+            "Austin": "austin",
+            "Boston": "boston",
+            "Charleston": "charleston",
+            "Chicago": "chicago",
+            "Cincinnati": "cincinnati",
+            "Cleveland": "cleveland",
+            "Dallas": "dallas",
+            "Denver": "denver",
+            "Houston": "houston",
+            "Honolulu": "honolulu",
+            "Las Vegas": "las-vegas",
+            "Los Angeles": "los-angeles",
+            "Miami": "miami",
+            "Milwaukee": "milwaukee",
+            "Minneapolis-Saint Paul": "minneapolis-saint-paul",
+            "Nashville": "nashville",
+            "New York": "new-york",
+            "Orlando": "orlando",
+            "Palo Alto": "palo-alto",
+            "Philadelphia": "philadelphia",
+            "Phoenix": "phoenix",
+            "Portland, OR": "portland-or",
+            "San Diego": "san-diego",
+            "San Francisco Bay Area": "san-francisco-bay-area",
+            "San Jose": "san-jose",
+            "Seattle": "seattle",
+            "St. Louis": "st-louis",
+            "Washington, D.C.": "washington-dc"
+        }
+
+        let searchCity = "";
+        // loop through city for search city
+        for (let key in apiCities) {
+            if (key === citySelected) {
+                searchCity = apiCities[key]
+                console.log(searchCity)
+            }
+        }
+
+        let queryURL = "https://api.teleport.org/api/urban_areas/slug%3A" + searchCity + "/scores/"
+
 
         // API 
         $.ajax({
