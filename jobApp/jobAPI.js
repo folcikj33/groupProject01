d35aa51a831d02c5842adb54acb110111c68eb06
mc0918 <mcampbell0918@gmail.com>
2019-03-14 11:27:53 -0500

    added list of job results for user specified inputs

--- jobApp/jobAPI.js
+++ jobApp/jobAPI.js
@@ -3,20 +3,37 @@
 //what's up with the %20? => it's UTF encoding for URLs to represent a empty space
 var keyword = "software development"; //this should end up being the .val for some user input
 var keywordEncoded = encodeURI(keyword);
-console.log(keywordEncoded);
+
+var where = "chicago";
+var locationEncoded = encodeURI(where);
+
+//if 1, only lists permanent positions (fullTime can be added to show both)
+var permanent = 0;
+//if 1, only lists full-time positions
+var fullTime = 1;
+//includes positions without listed salary
+var salary = 1;
+
 //distance in km
 var distance = 20;
 //max age of job posting in days
-var daysOld = 10;
+var daysOld = 30;
 
 var queryURL =
   "https://api.adzuna.com:443/v1/api/jobs/us/search/1?app_id=e6cd0ed5&app_key=0f19421e3255011b31ce0bf4464db591%09&results_per_page=10&what_phrase=" +
   keywordEncoded +
-  "&where=60611&distance=" +
+  "&where=" +
+  locationEncoded +
+  "&distance=" +
   distance +
   "&max_days_old=" +
   daysOld +
-  "&salary_include_unknown=1&full_time=1";
+  "&salary_include_unknown=" +
+  salary +
+  "&full_time=" +
+  fullTime +
+  "&permanent=" +
+  permanent;
 
 $.ajax({
   url: queryURL,
@@ -34,12 +51,27 @@ $.ajax({
   console.log(description);
   console.log(database.length);
 
-  //$("#test-box").html(company);
-
   for (i = 0; i < database.length; i++) {
     var companyList = database[i].company.display_name;
-    console.log(companyList);
-    //newDiv.append(companyList);
-    //$("#test-box").append(companyList);
+    var companyDiv = $("<div>").attr("class", "company-" + i);
+    companyDiv.append(companyList + ": ");
+
+    var titleList = database[i].title;
+    companyDiv.append(titleList + " ");
+
+    var description = database[i].description;
+    companyDiv.append("<br />" + description + "<br /> ");
+
+    var applyButton = $("<a>")
+      .attr("href", database[i].redirect_url)
+      .attr("target", "_blank")
+      .attr("class", "btn")
+      .attr("class", "btn-default")
+      .text("Apply!");
+    companyDiv.append(applyButton);
+
+    companyDiv.append("<hr />");
+
+    $("#jobs-here").append(companyDiv);
   }
 });
